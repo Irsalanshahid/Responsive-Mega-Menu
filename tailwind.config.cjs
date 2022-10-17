b2dbac839addf14eacce1dec184b6da1a97f914c
Arslan Shahid <irsalan.uetian08@gmail.com>
2022-10-17 21:19:37 +0500

    Creating Responsive Mega menu

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./*.{html,js,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/line-clamp"),
+    require("daisyui"),
+    require("prettier-plugin-tailwindcss")
+  ],
+};
